831bfe8bb27118f462e0be12149f5983911876a8
Hectordr03 <Hectordr03@gmail.com>
2020-10-03 16:44:14 -0400

    adjusting file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './App.module.css';
+import Cockpit from './containers/Cockpit'
 
 function App() {
   return (
     <div className={classes.App}>
       <h1>Hello world</h1>
+      <Cockpit />
     </div>
   );
 }
---
+++ src/containers/Cockpit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Cockpit.module.css';
+
+const cockpit = () => {
+	return (
+        <div className={ classes.cockpit }>
+            <h3>Cockpit component</h3>
+        </div>
+    );
+};
+
+export default cockpit;
