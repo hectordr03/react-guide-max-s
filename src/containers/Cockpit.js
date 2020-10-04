ca39823a6ef9b0d6cca246d7c0e9d7df721a3a7f
Hectordr03 <Hectordr03@gmail.com>
2020-10-04 06:54:54 -0400

    styling

--- src/containers/Cockpit.js
+++ src/containers/Cockpit.js
@@ -3,10 +3,10 @@ import classes from './Cockpit.module.css';
 
 const cockpit = () => {
 	return (
-        <div className={ classes.cockpit }>
-            <h3>Cockpit component</h3>
-        </div>
-    );
+		<div className={classes.cockpit}>
+			<h3 className={classes.title}>Cockpit component</h3>
+		</div>
+	);
 };
 
 export default cockpit;
