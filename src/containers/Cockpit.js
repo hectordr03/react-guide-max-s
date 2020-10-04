432ffec548a17a0145f830d7b3ca1cba1c789520
Hectordr03 <Hectordr03@gmail.com>
2020-10-04 07:46:40 -0400

    including sass with css modules

--- src/containers/Cockpit.js
+++ src/containers/Cockpit.js
@@ -2,9 +2,13 @@ import React from 'react';
 import classes from './Cockpit.module.css';
 
 const cockpit = () => {
+	const clicked = () => {
+		console.log('button was clicked');
+	};
 	return (
 		<div className={classes.cockpit}>
 			<h3 className={classes.title}>Cockpit component</h3>
+			<button className={classes.btn1} onClick={clicked}>I'm a button</button>
 		</div>
 	);
 };
